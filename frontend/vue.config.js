178e8fe40e4eebb8ee24afc2557b5e3d127b9c9f
mani-1986 <manivas81@gmail.com>
2025-03-03 15:45:48 -0500

    updated public path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/AI-Portfolio/frontend/dist/',
+  publicPath: '/',
   devServer: {
     proxy: {
       '/api': {
