48774b78f1c8a68501ded4525ffa5a2d6cbe6670
mani-1986 <manivas81@gmail.com>
2025-03-03 12:11:23 -0500

    Fix asset paths for deployment

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/AI-Portfolio/' : '/',
+  publicPath: '/AI-Portfolio/frontend/dist/',
   devServer: {
     proxy: {
       '/api': {
